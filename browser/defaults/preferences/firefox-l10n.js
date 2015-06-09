53943e44ae301e00ed0b1dcf0ec197fee40e64a9
Lei Chi <leichixian@outlook.com>
2015-06-09 14:02:30 +0800

    Upload locallized files

---
+++ browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,6 @@
+//@line 4 "c:\builds\moz2_slave\m-cen-w64-l10n-ntly-3-00000000\build\l10n\zh-CN\browser\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\m-cen-w64-l10n-ntly-3-00000000\build\l10n\zh-CN\browser\firefox-l10n.js"
+
+pref("general.useragent.locale", "zh-CN");
+pref("browser.startup.homepage", "chrome://browser/locale/browser.properties");
