b7428072184fef64b9d9ca74ee2026627a4b7bbb
Lei Chi <leichixian@outlook.com>
2015-06-09 14:33:47 +0800

    Upload files.

---
+++ browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,5 @@
+//@line 4 "c:\builds\moz2_slave\m-cen-w64-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
+
+//@line 6 "c:\builds\moz2_slave\m-cen-w64-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
+
+pref("general.useragent.locale", "en-US");
