42de06060f0f88f89abd68f074dd0e3234d76fd8
Lei Chi <leichixian@outlook.com>
2015-06-16 23:16:07 +0800

    更新翻译，确认换行符为unix格式

--- browser/defaults/preferences/firefox-l10n.js
+++ browser/defaults/preferences/firefox-l10n.js
@@ -1,6 +1,6 @@
-//@line 4 "c:\builds\moz2_slave\m-cen-w64-l10n-ntly-3-00000000\build\l10n\zh-CN\browser\firefox-l10n.js"
+//@line 4 "/builds/slave/m-cen-lx-ntly-0000000000000000/build/src/browser/locales/en-US/firefox-l10n.js"
 
-//@line 6 "c:\builds\moz2_slave\m-cen-w64-l10n-ntly-3-00000000\build\l10n\zh-CN\browser\firefox-l10n.js"
+//@line 6 "/builds/slave/m-cen-lx-ntly-0000000000000000/build/src/browser/locales/en-US/firefox-l10n.js"
 
 pref("general.useragent.locale", "zh-CN");
 pref("browser.startup.homepage", "chrome://browser/locale/browser.properties");
