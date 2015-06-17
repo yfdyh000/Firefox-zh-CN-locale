95f55b64b1a3f391d1efe3dc92c0a6c3a29d6293
Lei Chi <leichixian@outlook.com>
2015-06-17 23:30:49 +0800

    更新语言包

--- browser/defaults/preferences/firefox-l10n.js
+++ browser/defaults/preferences/firefox-l10n.js
@@ -1,6 +1,6 @@
-//@line 4 "/builds/slave/m-cen-lx-ntly-0000000000000000/build/src/browser/locales/en-US/firefox-l10n.js"
+//@line 4 "c:\builds\moz2_slave\m-cen-w32-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
 
-//@line 6 "/builds/slave/m-cen-lx-ntly-0000000000000000/build/src/browser/locales/en-US/firefox-l10n.js"
+//@line 6 "c:\builds\moz2_slave\m-cen-w32-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
 
 pref("general.useragent.locale", "zh-CN");
 pref("browser.startup.homepage", "chrome://browser/locale/browser.properties");
