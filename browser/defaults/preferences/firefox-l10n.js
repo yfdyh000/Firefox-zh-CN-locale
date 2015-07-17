0204b54fa41ce8c152c1ff0e4459766d8387a530
lychichem <leichixian@outlook.com>
2015-07-18 00:06:53 +0800

    Update language files.

--- browser/defaults/preferences/firefox-l10n.js
+++ browser/defaults/preferences/firefox-l10n.js
@@ -2,4 +2,7 @@
 
 //@line 6 "c:\builds\moz2_slave\m-cen-w64-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
 
+//@line 9 "c:\builds\moz2_slave\m-cen-w64-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
+pref("browser.search.geoSpecificDefaults", true);
+
 pref("general.useragent.locale", "en-US");
