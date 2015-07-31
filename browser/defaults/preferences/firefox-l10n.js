ccf62716713d200d1e7872e3a7009cb7b89182d9
lychichem <leichixian@outlook.com>
2015-07-31 23:36:07 +0800

    更新翻译

--- browser/defaults/preferences/firefox-l10n.js
+++ browser/defaults/preferences/firefox-l10n.js
@@ -1,8 +1,8 @@
-//@line 4 "c:\builds\moz2_slave\m-cen-w32-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
+//@line 4 "c:\builds\moz2_slave\m-cen-w64-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
 
-//@line 6 "c:\builds\moz2_slave\m-cen-w32-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
+//@line 6 "c:\builds\moz2_slave\m-cen-w64-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
  
-//@line 9 "c:\builds\moz2_slave\m-cen-w32-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
+//@line 9 "c:\builds\moz2_slave\m-cen-w64-ntly-000000000000000\build\src\browser\locales\en-US\firefox-l10n.js"
 pref("browser.search.geoSpecificDefaults", true);
 
 pref("general.useragent.locale", "zh-CN");
